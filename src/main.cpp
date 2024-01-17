60c853b417fa8f157d78059d11f65086d579b2de
alexkaasik <aleks.kaasik@outlook.com>
2024-01-17 03:33:21 +0200

    Added function to controll led mode by buttons

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,9 @@
 #define LED_PIN     8
 #define NUM_LEDS    30
 
+#define FADE_PIN    7
+#define BLINK_PIN    6
+
 int rbg_0[][3] = { 
   {255,   000,    000}, // Red
   {255,   128,    000}, // Orange
@@ -25,50 +28,80 @@ int len0 = sizeof(rbg_0)/sizeof(rbg_0[0]);
 int len1 = sizeof(rbg_1)/sizeof(rbg_1[0]);
 
 int t;
+int aba;
 
 CRGB leds[NUM_LEDS];
 
 void setup() {
   FastLED.addLeds<WS2812, LED_PIN, GRB>(leds, NUM_LEDS);
+  pinMode(FADE_PIN, INPUT);
+  pinMode(BLINK_PIN, INPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
-  
-  for (int x = 0; x < len0; x++){
-    for ( int i = 0; i < NUM_LEDS; i++ ) {
-      leds[i] = CRGB(rbg_0[x][0], rbg_0[x][1], rbg_0[x][2]);
-      FastLED.show();
-    }
-    delay(1000);
-  }
-  
-  /*
-  for (int x = 0; x < len1; x++){ 
-    for (int y = 0; y < 256; y++){
-      rbg_1[x] = y;
-      // Changes color of rows of led's
-      for ( int i = 0; i < NUM_LEDS; i++ ) {
-        leds[i] = CRGB(rbg_1[0], rbg_1[1], rbg_1[2]);
-        FastLED.show();
-      }
-    }
-    
-    t = x - 1;
-    if ( t < 0 ){
-      t = len1 - 1;
-    }
 
-    if ( rbg_1[t] != 0 ){
-      for (int y = 255; y >= 0; y--){
-        rbg_1[t] = y;
-        // Changes color of rows of led's
-        for ( int i = 0; i < NUM_LEDS; i++ ) {
-          leds[i] = CRGB(rbg_1[0], rbg_1[1], rbg_1[2]);
-          FastLED.show();
-        }
-      }
-    }
-    delay(100);
+
+
+  if (digitalRead(BLINK_PIN) == LOW) {
+    aba = 0;
   }
-  */
+  else if (digitalRead(FADE_PIN) == LOW) {
+    aba = 1;
+  }
+
+  Serial.println(aba);
+
+  switch (aba){
+    case 0: // BLINK
+      for (int x = 0; x < len0; x++){
+        for ( int i = 0; i < NUM_LEDS; i++ ) {
+          leds[i] = CRGB(rbg_0[x][0], rbg_0[x][1], rbg_0[x][2]);
+          FastLED.show();
+          if (digitalRead(FADE_PIN) == LOW){ break; }
+        }
+        if (digitalRead(FADE_PIN) == LOW){ break; }
+        delay(1000);
+      }
+      break;
+  
+    case 1: // FADE
+      for (int x = 0; x < len1; x++){ 
+        for (int y = 0; y < 256; y++){
+          rbg_1[x] = y;
+          // Changes color of rows of led's
+          for ( int i = 0; i < NUM_LEDS; i++ ) {
+            leds[i] = CRGB(rbg_1[0], rbg_1[1], rbg_1[2]);
+            FastLED.show();
+            if (digitalRead(BLINK_PIN) == LOW){ break; }
+          }
+          if (digitalRead(BLINK_PIN) == LOW){ break; }
+        }   
+        if (digitalRead(BLINK_PIN) == LOW){ break; }
+        t = x - 1;
+        if ( t < 0 ){
+          t = len1 - 1;
+        }
+
+        if ( rbg_1[t] != 0 ){
+          for (int y = 255; y >= 0; y--){
+            rbg_1[t] = y;
+            // Changes color of rows of led's
+            for ( int i = 0; i < NUM_LEDS; i++ ) {
+              leds[i] = CRGB(rbg_1[0], rbg_1[1], rbg_1[2]);
+              FastLED.show();
+              if (digitalRead(BLINK_PIN) == LOW){ break; }
+            }
+            if (digitalRead(BLINK_PIN) == LOW){ break; }
+          }
+        
+        }
+        if (digitalRead(BLINK_PIN) == LOW){ break; }
+        delay(100);
+      }
+      break;
+
+    default:
+      break;
+  }
 }
